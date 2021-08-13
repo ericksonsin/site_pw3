54f352244837c279e1aaf0608299a9ce0fc7a14f
erick sonsin <kciire@live.com>
2021-08-12 22:22:35 -0300

    12.08

--- recupera-senha.php
+++ recupera-senha.php
@@ -8,61 +8,45 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon/pw3.ico">
-
-    <title>PW3</title>
-
+    <title>Recupera senha - PW3</title>
     <!-- Principal CSS do Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
-
     <!-- Estilos customizados para esse template -->
     <link href="css/starter-template.css" rel="stylesheet">
-
     <!-- FONTAWESOME -->
     <script src="https://kit.fontawesome.com/77f3dd62a7.js" crossorigin="anonymous"></script>
 </head>
 
 <body cz-shortcut-listen="true" class="bg-secondary">
-
     <main role="main" class="container">
-
         <div class="row">
-            <div class="col-sm-4 offset-sm-4 border bg-white">
-                <h1 class="text-center">
-                    <a href="index.php">PW3</a>
+            <div class="col-sm-4 offset-sm-4 bg-white mt-5 border rounded bg-white">
+                <h1 class="h4 text-center">
+                    <a href="index.php">PW III</a>
                 </h1>
-                <p class="text-center">Informe seu e-mail para recuperar a senha</p>
-
-                <form action="" method="">
-
+                <p class="login-box-msg text-center">Informe seu e-mail para recuperar a senha</p>
+                <form action="validaLogin.php" method="post">
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
                         </div>
                         <input type="mail" name="email" class="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1">
                     </div>
-
                     <div class="form-group text-right">
-                        <button class="btn btn-primary">Recuperar</button>
+                        <button type="submit" class="btn btn-primary">Recuperar</button>
                     </div>
-
-                    <p>
-                        <a href="login.php">Fazer login</a>
-                    </p>
-
                 </form>
-
+                <p class="mb-1">
+                    <a href="login.php">Fazer login</a>
+                </p>
             </div>
         </div>
-
     </main><!-- /.container -->
-
     <!-- Principal JavaScript do Bootstrap
     ================================================== -->
     <!-- Foi colocado no final para a página carregar mais rápido -->
     <script src="js/jquery-3.js"></script>
     <script src="js/bootstrap.js"></script>
-
-
 </body>
 
 </html>
